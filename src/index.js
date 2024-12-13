ae082970d68f0f4ddc36fe28682aebc5178fce01
Baker Umar <49150727+Baker0707@users.noreply.github.com>
2024-12-13 23:37:43 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './containers/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit"; // Use combineReducers for multiple reducers
+import { searchRobotsReducer, requestRobots } from "./reducers"; // Import reducers
+import { createLogger } from "redux-logger";
+import { thunk } from "redux-thunk"; // Use the named export 'thunk'
+import "./index.css";
+import App from "./containers/App";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
 
-import reportWebVitals from './reportWebVitals';
-import 'tachyons';
-import { robots } from './robots';
+// Create a logger middleware
+const logger = createLogger();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Combine multiple reducers
+const rootReducer = combineReducers({
+  searchRobots: searchRobotsReducer,
+  requestRobots: requestRobots,
+});
+
+// Create the Redux store using configureStore and add logger middleware
+const store = configureStore({
+  reducer: rootReducer, // Use the combined reducers
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App robots={robots} />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+ServiceWorker.register();
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
